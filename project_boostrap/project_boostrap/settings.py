d6a04448be848b2938841e2b6954e44a91759c6b
Pedro Souza <pedrocontato1840@gmail.com>
2023-05-26 21:12:51 -0300

    gitignore commit

--- project_boostrap/project_boostrap/settings.py
+++ project_boostrap/project_boostrap/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@j20j2n(i%d-)ahh*-wn^^ilwcuad0$qp=ha+q$@al4l8_x2t-'
+dotenv.load_dotenv()
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
